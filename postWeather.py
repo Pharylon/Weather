56be66509e529a7feea2565877e062615baa30a1
Pharylon <me@zachshuford.com>
2015-05-29 08:40:02 -0400

    Fixed weather readings'

--- postWeather.py
+++ postWeather.py
@@ -7,7 +7,7 @@ def getReadings():
     humidity, dht_temp = Adafruit_DHT.read_retry(22, 4)
     if humidity is not None and dht_temp is not None:
         bmp_sensor = BMP085.BMP085()
-        pressure = bmp_sensor.read_pressure
+        pressure = bmp_sensor.read_pressure()
         bmp_temp = bmp_sensor.read_temperature()
         if pressure is not None and bmp_temp is not None:
             data = {}
@@ -20,5 +20,5 @@ def getReadings():
 
 
 data = getReadings()
-#print(data)
+print(data)
 requests.post('http://pharylonapi.azurewebsites.net/api/weather/reading', data)
